997999d311c4e83aedbbd8ac67a1ad38f9d24e60
David H Hagan <david@davidhhagan.com>
2014-02-15 22:29:35 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+# /ACT/setup.py
+
+from setuptools import setup
+
+setup(name='ACT',
+      version='0.0.1',
+      description='Python library for analyzing trace gas data from thermo scientific gas analyzers',
+      url='http://github.com/dhhagan/ACT',
+      author='David H Hagan',
+      author_email='david@davidhhagan.com',
+      license='MIT',
+      packages=['ACT'],
+      zip_safe=False)
